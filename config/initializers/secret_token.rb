cbe1a9e043c429cc878aebf2134e7e419c8f6006
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-12-13 20:17:48 +0900

    rails new queuechan-api -d mysql --skip-bundle -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QueuechanApi::Application.config.secret_key_base = 'de395101faa371d5dff476abfa73dc4235ab75c2c2f550a84f50bac7ebc5eea938e04f24b8968bf12fb27085b5d7f2bf9bc2b4c31b58aedb923e73607f7cbd74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QueuechanApi::Application.config.session_store :cookie_store, key: '_queuechan-api_session'
