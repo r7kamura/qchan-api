6edd056d3aa540d984fdb01ad662a4e9d54c1f5e
Ryo Nakamura <r7kamura@gmail.com>
2013-12-14 03:51:37 +0900

    Add association: job.children

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ActiveRecord::Base
+  has_many :builds, dependent: :destroy
+  has_many :children, source: :child, through: :job_dependencies
+  has_many :job_dependencies, foreign_key: :parent_id
 end
--- app/models/job_dependency.rb
+++ app/models/job_dependency.rb
@@ -1,2 +1,3 @@
 class JobDependency < ActiveRecord::Base
+  belongs_to :child, class_name: :Job
 end
