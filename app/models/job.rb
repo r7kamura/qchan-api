e762b33f1b5d4c5c0b6d0f987168965d86f3e3de
Ryo Nakamura <r7kamura@gmail.com>
2013-12-14 15:24:23 +0900

    Add destroy dependency to Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ActiveRecord::Base
   has_many :builds, dependent: :destroy
-  has_many :children, source: :child, through: :job_dependencies
-  has_many :job_dependencies, foreign_key: :parent_id
+  has_many :children, source: :child, through: :job_dependencies, dependent: :destroy
+  has_many :job_dependencies, foreign_key: :parent_id, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
 end
