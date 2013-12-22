307d510f25376d6fb5628d31c5803de6475dd4ce
Ryo Nakamura <r7kamura@gmail.com>
2013-12-22 09:41:27 +0900

    fakeredis/rspec seems to be already enabled

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "webmock/rspec"
-require "fakeredis/rspec"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
